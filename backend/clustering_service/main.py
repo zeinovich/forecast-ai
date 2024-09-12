71343b378955ddc876d0172268cad29a400db710
Nikita.Zinovich <nikita.zinovich@gmail.com>
2024-09-12 12:32:03 +0300

    [interim] Clustering for default data

--- backend/clustering_service/main.py
+++ backend/clustering_service/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import hdbscan
 from tslearn.metrics import cdist_dtw
-
+import pandas as pd
 from utils import decode_dataframe, encode_dataframe
 
 app = FastAPI()
@@ -16,25 +16,34 @@ async def clusterize(payload: dict):
     """
     # Декодируем данные из base64 в CSV
     data = payload["data"]
-    df = decode_dataframe(data)
+
+    df = decode_dataframe(data)[["date", "item_id", "cnt"]]
+
+    items = df["item_id"].unique()
+    items_ts = {}
+
+    for item in items:
+        items_ts[item] = df[df["item_id"] == item]["cnt"].to_list()
+
+    items_ts = pd.DataFrame.from_dict(items_ts, orient="index")
 
     # Подготовка данных (временные ряды) для кластеризации
-    time_series = (
-        df.to_numpy()
-    )  # предполагается, что данные представляют временные ряды
+    # предполагается, что данные представляют временные ряды
 
     # Рассчитываем матрицу расстояний DTW
-    distance_matrix = cdist_dtw(time_series)
+    distance_matrix = cdist_dtw(items_ts)
 
     # Применяем HDBSCAN для кластеризации
     clusterer = hdbscan.HDBSCAN(metric="precomputed", min_cluster_size=2)
     labels = clusterer.fit_predict(distance_matrix)
 
     # Добавляем метки кластеров к DataFrame
-    df["cluster"] = labels
+    items_ts["cluster"] = labels
+    items_ts.index.name = "item_id"
+    items_ts = items_ts.reset_index()
 
     # Кодируем результат обратно в base64
-    encoded_result = encode_dataframe(df)
+    encoded_result = encode_dataframe(items_ts[["item_id", "cluster"]])
 
     return {
         "encoded_dataframe": encoded_result,
